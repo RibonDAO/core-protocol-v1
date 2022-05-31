18fa329821e70eb11d7ad8669631a35b2b236245
Juju <juju@ribon.io>
2022-05-31 09:30:20 -0300

    Add balancer interface

---
+++ contracts/interfaces/IBPool.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.14;
+
+interface IBPool {
+    function getSpotPrice(address tokenIn, address tokenOut) external view returns (uint spotPrice);
+
+    function swapExactAmountOut(
+        address tokenIn,
+        uint maxAmountIn,
+        address tokenOut,
+        uint tokenAmountOut,
+        uint maxPrice
+    )external returns (uint tokenAmountIn, uint spotPriceAfter);
+}
+
+
